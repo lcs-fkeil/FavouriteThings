2b6495d1489fdda8b12b21bdd8b72b71b18decd0
Franka Keil <fkeil@lcs.on.ca>
2021-10-22 08:54:38 -0400

    Added images of my three favourite views, and got them to show up in phone preview

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,19 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Franka Keil on 2021-10-20.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+
+        }
+    }
+}
+
---
+++ FavouriteThings/ThirdFavouriteView.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdFavouriteView.swift
+//  FavouriteThings
+//
+//  Created by Franka Keil on 2021-10-20.
+//
+
+import SwiftUI
+
+struct ThirdFavouriteView: View {
+    var body: some View {
+        Image ("Klara")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct ThirdFavouriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdFavouriteView()
+    }
+}
